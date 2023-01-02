b5be9c14cb93c99012ca058aef24a2bbc741ed2f
Antonio Lázaro <antoniolazaro@me.com>
2023-01-02 20:25:19 +0100

    Change email verification message

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -53,39 +53,8 @@ const sendVerificationEmail = async (to: string, token: string) => {
   const subject = 'Email Verification';
   // replace this url with the link to the email verification page of your front-end app
   const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
-  const text = `
-  <!DOCTYPE htmlPUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-  <html lang="en">
-  
-    <head>
-      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-    </head>
-    <div style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">Log in with this magic link<div></div>
-    </div>
-    <table style="width:100%;background-color:#ffffff" align="center" border="0" cellPadding="0" cellSpacing="0" role="presentation">
-      <tbody>
-        <tr>
-          <td>
-            <div><!--[if mso | IE]>
-              <table role="presentation" width="100%" align="center" style="max-width:37.5em;padding-left:12px;padding-right:12px;margin:0 auto;"><tr><td></td><td style="width:37.5em;background:#ffffff">
-            <![endif]--></div>
-            <div style="max-width:37.5em;padding-left:12px;padding-right:12px;margin:0 auto">
-              <p style="font-size:24px;line-height:24px;margin:40px 0;color:#333;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;font-weight:bold;padding:0">Login</p><a target="_blank" style="color:#2754C5;text-decoration:underline;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;font-size:14px;display:block;margin-bottom:16px" href="${verificationEmailUrl}">Click here to log in with this magic link</a>
-              <p style="font-size:14px;line-height:24px;margin:24px 0;color:#333;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;margin-bottom:14px">Or, copy and paste this temporary login code:</p><code style="display:inline-block;padding:16px 4.5%;width:90.5%;background-color:#f4f4f4;border-radius:5px;border:1px solid #eee;color:#333">sparo-ndigo-amurt-secan</code>
-              <p style="font-size:14px;line-height:24px;margin:24px 0;color:#ababab;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;margin-top:14px;margin-bottom:16px">If you didn&#x27;t try to login, you can safely ignore this email.</p>
-              <p style="font-size:14px;line-height:24px;margin:24px 0;color:#ababab;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;margin-top:12px;margin-bottom:38px">Hint: You can set a permanent password in Settings &amp; members → My account.</p><img alt="Notion&#x27;s Logo" src="https://react-email-demo-7nbdzn72d-resend.vercel.app/static/notion-logo.png" width="32" height="32" style="display:block;outline:none;border:none;text-decoration:none" />
-              <p style="font-size:12px;line-height:22px;margin:16px 0;color:#898989;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;margin-top:12px;margin-bottom:24px"><a target="_blank" style="color:#898989;text-decoration:underline;font-family:-apple-system, BlinkMacSystemFont, &#x27;Segoe UI&#x27;, &#x27;Roboto&#x27;, &#x27;Oxygen&#x27;, &#x27;Ubuntu&#x27;, &#x27;Cantarell&#x27;, &#x27;Fira Sans&#x27;, &#x27;Droid Sans&#x27;, &#x27;Helvetica Neue&#x27;, sans-serif;font-size:14px" href="https://notion.so">Notion.so</a>, the all-in-one-workspace<br />for your notes, tasks, wikis, and databases.</p>
-            </div>
-            <div><!--[if mso | IE]>
-            </td><td></td></tr></table>
-            <![endif]--></div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  
-  </html>
-  `;
+  const text = `Dear user,
+To verify your email, click on this link: ${verificationEmailUrl}`;
   await sendEmail(to, subject, text);
 };
 
