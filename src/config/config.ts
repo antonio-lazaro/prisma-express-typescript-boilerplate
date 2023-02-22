dbfc12c004e23a78f267a48d029828055552d2dd
Antonio Lázaro <antoniolazaro@me.com>
2023-02-22 20:36:38 +0100

    Modify execution in production environment

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 import Joi from 'joi';
 
-dotenv.config({ path: path.join(__dirname, '../../.env') });
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 const envVarsSchema = Joi.object()
   .keys({
